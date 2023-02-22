84fb970d526922ec30e8a9bba2b076afa692db6d
Kondrat <yo126@yandex.ru>
2023-02-22 21:30:11 +0300

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 21);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+void Srednee(int[,] matrix)
+{
+    int sum;
+    System.Console.Write("Среднее арифмитическое столбцов = ");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            
+            sum += matrix[i, j];
+            System.Console.Write(Math.Round((double)sum / matrix.GetLength(0), 1));
+
+            if (j < matrix.GetLength(1) - 1)
+            {
+                System.Console.Write("; ");
+            }
+            else
+            {
+                System.Console.Write(".");
+            }
+        }
+    }
+}
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов матрицы: ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+Srednee(myMatrix);
